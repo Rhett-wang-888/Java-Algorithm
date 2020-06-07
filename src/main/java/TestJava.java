6a24ef7efc80007102f22aeaf105f96d17e1f13c
wanzgheng <120445143@qq.com>
2020-06-07 14:42:22 +0800

    test

--- src/main/java/TestJava.java
+++ src/main/java/TestJava.java
@@ -1,5 +1,12 @@
 
 public class TestJava {
+    class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            data=this.data;
+        }
+    }
     public static void main(String[] args) {
 
     }
