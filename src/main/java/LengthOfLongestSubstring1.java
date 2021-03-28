b1bb913e4498f8a8b888b8257383a127f5180268
Rhett-wang-888 <120445143@qq.com>
2021-03-28 10:18:27 +0800

    test

---
+++ src/main/java/LengthOfLongestSubstring1.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Rhett
+ * @title: LengthOfLongestSubstring1
+ * @description: TODO 无重复字符的最长字符串
+ * @date 2021/3/28 9:07
+ */
+public class LengthOfLongestSubstring1 {
+    public static void main(String[] args) {
+
+
+    }
+    private static int LengthOfLongestSubstring(String s){
+        Set<Character> occ= new HashSet<Character>();
+        int n=s.length();
+        int rk =-1,ans=0;
+
+        for(int i=0;i<n;++i){
+            if(i  !=0){
+                occ.remove(s.charAt(i-1));
+            }
+            while(rk+1<n && !occ.contains(s.charAt(rk+1))){
+                occ.add(s.charAt(rk+1));
+                ++rk;
+            }
+
+            ans=Math.max(ans,rk-i+1);
+        }
+
+        return ans;
+    }
+}
+
