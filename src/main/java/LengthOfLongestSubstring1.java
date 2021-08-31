dcff7276cd81ee3ce6f02510e1cc4b2cc3a2adda
Rhett-wang-888 <120445143@qq.com>
2021-08-31 17:17:56 +0800

    test

--- src/main/java/LengthOfLongestSubstring1.java
+++ src/main/java/LengthOfLongestSubstring1.java
@@ -31,5 +31,22 @@ public class LengthOfLongestSubstring1 {
 
         return ans;
     }
+    public int lengthOfLongestSubstring1(String s){
+        Set<Character> sets= new HashSet<Character>();
+        int n=s.length();
+        int rk=-1,ans=0;
+
+        for(int i=0;i<n;++i){
+            if(i !=0){
+                sets.remove(s.charAt(i-1));
+            }
+            while(rk+1<n && !sets.contains(s.charAt(rk+1))){
+                sets.add(s.charAt(rk+1));
+                ++rk;
+            }
+            ans=Math.max(ans,rk-i+1);
+        }
+        return ans;
+    }
 }
 
