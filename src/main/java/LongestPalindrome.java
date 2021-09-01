5bb63ef1a12bce3ed2068c9ab008148b7c262a3d
Rhett-wang-888 <120445143@qq.com>
2021-09-01 15:46:19 +0800

    test

--- src/main/java/LongestPalindrome.java
+++ src/main/java/LongestPalindrome.java
@@ -90,4 +90,65 @@ public class LongestPalindrome {
         }
         return s.substring(maxEnd-maxLen+1,maxEnd+1);
     }
+
+    public String LongestPalindrome3(String s){
+        if(s==null||s.length()==0){
+            return "";
+        }
+        int strLen=s.length();
+        int left=0;
+        int right=0;
+        int len=1;
+        int maxStart=0;
+        int maxLen=0;
+
+        for(int i=0;i<strLen;i++){
+            left=i-1;
+            right=i+1;
+            while(left>=0 &&s.charAt(left)==s.charAt(i)){
+                len++;
+                left--;
+            }
+            while(right<strLen&&s.charAt(right)==s.charAt(i)){
+                len++;
+                right++;
+            }
+            while(left>=0&&right<strLen&&s.charAt(right)==s.charAt(left)){
+                len=len+2;
+                left--;
+                right++;
+            }
+            if(len>maxLen){
+                maxLen=len;
+                maxStart=left;
+            }
+            len=1;
+        }
+        return s.substring(maxStart+1,maxStart+maxLen+1);
+    }
+
+    public String longestPalindrome4(String s){
+        if(s==null||s.length()<2) return s;
+        int strLen=s.length();
+        int maxStart=0;
+        int maxEnd=0;
+        int maxLen=0;
+
+        boolean[][] dp=new boolean[strLen][strLen];
+
+        for(int r=1;r<strLen;r++){
+            for(int l=0;l<r;l++){
+                if(s.charAt(l)==s.charAt(r)&&(r-1<=2 ||dp[l+1][r-1])){
+                    dp[l][r]=true;
+                    if(r-l+1>maxLen){
+                        maxLen=r-l+1;
+                        maxStart=1;
+                        maxEnd=r;
+                    }
+                }
+            }
+        }
+
+        return s.substring(maxStart,maxEnd+1);
+    }
 }
