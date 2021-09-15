ef3e8e52622d9d4dbe7be608d3a0b4980de930d6
Rhett-wang-888 <120445143@qq.com>
2021-09-15 14:50:46 +0800

    test

--- src/main/java/RemoveNthFromEnd.java
+++ src/main/java/RemoveNthFromEnd.java
@@ -1,3 +1,7 @@
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @author Rhett
  * @title: RemoveNthFromEnd
@@ -7,6 +11,48 @@
 public class RemoveNthFromEnd {
     public static void main(String[] args) {
 
+    }
+
+    public ListNode removeNthFormEnd2(ListNode head,int n){
+        ListNode dummy=new ListNode(0);
+        dummy.next=head;
+        Deque<ListNode> stack= new LinkedList<ListNode>();
+        ListNode cur=dummy;
+
+        while(cur !=null){
+            stack.push(cur);
+            cur=cur.next;
+        }
+        for(int i=0;i<n;++i){
+            stack.pop();
+        }
+        ListNode prev=stack.peek();
+        prev.next=prev.next.next;
+
+        ListNode ans=dummy.next;
+
+        return ans;
+    }
+    public ListNode removeNthFromEnd1(ListNode head,int n){
+        ListNode dummy=new ListNode(0);
+        int length=getLength1(head);
+        ListNode cur=dummy;
+
+        for(int i=0;i<length-n+1;++i){
+            cur=cur.next;
+        }
+        cur.next=cur.next.next;
+        ListNode ans=dummy.next;
+
+        return ans;
+    }
+    public Integer getLength1(ListNode head){
+        int length=0;
+        while (head!=null){
+            ++length;
+            head=head.next;
+        }
+        return 0;
     }
 
     public ListNode removeNthFromEnd(ListNode head,int n){
