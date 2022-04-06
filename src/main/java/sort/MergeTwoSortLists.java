8462b04c4ec84a52f58ee45528a6056462d7c69f
Rhett-wang-888 <120445143@qq.com>
2022-04-06 11:38:57 +0800

    test

--- src/main/java/sort/MergeTwoSortLists.java
+++ src/main/java/sort/MergeTwoSortLists.java
@@ -1,5 +1,7 @@
 package sort;
 
+import com.sun.corba.se.pept.transport.ListenerThread;
+
 /**
  * @author Rhett
  * @title: MergeTwoSortLists
@@ -41,5 +43,128 @@ public class MergeTwoSortLists {
 
          return prehead.next;
 
+    }
+
+    public ListNode mergeTwoLists6(ListNode l1,ListNode l2){
+        ListNode prehead=new ListNode(-1);
+        ListNode prev=prehead;
+        while(l1 !=null && l2 !=null){
+            if(l1.data<=l2.data){
+                prev.next=l1;
+                l1=l1.next;
+            }else{
+                prev.next=l2;
+                l2=l2.next;
+            }
+            prev=prev.next;
+        }
+        prev.next=l1==null?l2:l1;
+
+        return prehead.next;
+    }
+    public ListNode mergeTwoLists7(ListNode l1,ListNode l2){
+        if(l1==null&& l2==null) return null;
+        if(l1==null || l2 ==null)return l1==null?l2:l1;
+
+        ListNode curr_l1=l1;
+        ListNode curr_l2=l2;
+
+        ListNode newHead=null;
+        if(curr_l1.data<curr_l2.data){
+            newHead=curr_l1;
+            curr_l1=curr_l1.next;
+        }else{
+            newHead=curr_l2;
+            curr_l2=curr_l2.next;
+        }
+
+        ListNode resHead=newHead;
+
+        while(curr_l1 !=null && curr_l2 !=null){
+            if(curr_l1.data<curr_l1.data){
+                newHead.next=curr_l1;
+                curr_l1=curr_l1.next;
+            }else{
+                newHead.next=curr_l2;
+                curr_l2=curr_l2.next;
+            }
+            newHead=newHead.next;
+        }
+
+        while(curr_l1 !=null){
+            newHead.next=curr_l1;
+            curr_l1=curr_l1.next;
+            newHead=newHead.next;
+        }
+        while(curr_l2 !=null){
+            newHead.next=curr_l2;
+            curr_l2=curr_l2.next;
+            newHead=newHead.next;
+        }
+
+        return resHead;
+    }
+    public ListNode mergeTwoLists9(ListNode l1,ListNode l2){
+        if(l1==null)return l2;
+        if(l2==null)return l1;
+
+        ListNode dummy= new ListNode(0);
+        ListNode p=dummy;
+
+        while(l1 !=null && l2 !=null){
+            if(l1.data<l2.data){
+                p.next=l1;
+                l1=l1.next;
+            }else{
+                p.next=l2;
+                l2=l2.next;
+            }
+            p=p.next;
+        }
+
+        while(l1!=null){
+            p.next=l1;
+            l1=l1.next;
+            p=p.next;
+        }
+        while(l2 !=null){
+            p.next=l2;
+            l2=l2.next;
+            p=p.next;
+        }
+        return dummy.next;
+    }
+
+    public ListNode mergeTwoList10(ListNode l1,ListNode l2){
+        if (l1==null)return l2;
+        if(l2==null) return l1;
+
+        ListNode dummy=new ListNode(0);
+        ListNode p=dummy;
+
+        while(l1 !=null && l2 != null){
+            if(l1.data<l2.data){
+                p.next=l1;
+                l1=l1.next;
+            }else{
+                p.next=l2;
+                l2=l2.next;
+            }
+            p=p.next;
+        }
+
+        while(l1 !=null){
+            p.next=l1;
+            l1=l1.next;
+            p=p.next;
+        }
+
+        while(l2 !=null){
+            p.next=l2;
+            l2=l2.next;
+            p=p.next;
+        }
+
+        return dummy.next;
     }
 }
